49a6b165b30ef7bd246bdd9963465a329209f0e1
FrancisSI19 <tauan_19@hotmail.com>
2022-02-15 12:45:54 -0300

    Estilizando tela de registros cadastrados

--- telas/tab-bar/List.js
+++ telas/tab-bar/List.js
@@ -1,24 +1,48 @@
 import React from "react";
-import { Text, Image, StyleSheet } from "react-native";
+import { Text, Image, StyleSheet, View } from "react-native";
 
 function List({date, image, time, atividade1, atividade2, atividade3, relatorio}) {
-  return <>
-    <Text>{date}</Text>
+  return (
+    <View style={styledList.container}>
+      <Text style={styledList.title}>{date}</Text>
+      <Image style={{ width: 60, height: 60 }} source={image}/>
+      <Text style={styledList.title}>{time}</Text>
+      <Text style={styledList.title}>{atividade1}</Text>
+      <Text style={styledList.title}>{atividade2}</Text>
+      <Text style={styledList.title}>{atividade3}</Text>
+      <Text numberOfLines={2} style={styledList.description}>{relatorio}</Text>
+    </View>
     
-    <Text>{time}</Text>
-    <Text>{atividade1}</Text>
-    <Text>{atividade2}</Text>
-    <Text>{atividade3}</Text>
-    <Text>{relatorio}</Text>
-  </> 
+  )
 }
-const styles = StyleSheet.create({
+const styledList = StyleSheet.create({
     sectionContainer: {
     backgroundColor: `#4169e1`,
     width: "100%",
     height: "100%",   
   },
 
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#6F4E37",
+    marginBottom: 16,
+  },
+  description: {
+    fontSize: 16,
+    color: "#000",
+  },
 });
 
 export default List;
--- telas/tab-bar/RegisterDaily.js
+++ telas/tab-bar/RegisterDaily.js
@@ -1,32 +1,52 @@
 import React from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { keyExtractor } from "react-native/Libraries/Lists/VirtualizeUtils";
 import List from "./List";
-import { Image } from "react-native";
-import emotionOpacity from '../../telas/assets/humores/emotionOpacity.png';
 
+const confused = require("../../telas/assets/humores/confused.png");
+const happy = require("../../telas/assets/humores/happy.png");
+const sleeping = require("../../telas/assets/humores/sleeping.png");
+const nervous = require("../../telas/assets/humores/nervous.png");
 const myRegister = [
   {
     id: "1",
     date: "14 de Fevereiro de 2022",
-    image: "source={emotionOpacity}",
+    image: confused,
     time: "07:00",
     atividade1: "Meditação",
     atividade2: "Esporte",
     atividade3: "Cozinhar",
-    relatorio: "Hoje acordei, li a bíblia, orei, fui para a academia, fiz um delicioso almoço"
+    relatorio: "Hoje acordei, li a bíblia, orei, fui para a academia, fiz um delicioso almoço."
+
+  },
+  {
+    id: "2",
+    date: "15 de Fevereiro de 2022",
+    image: happy,
+    time: "07:00",
+    atividade1: "Estudo",
+    atividade2: "Casa",
+    atividade3: "Esporte",
+    relatorio: "Hoje acordei, estudei, arrumei a casa, fui para a academia."
 
   }
+
 ];
 
 
 function RegisterDaily() {
+  /* função renderItem criada fora do flatlist
+  para não precisar recriar toda vez que a lista for renderizada. */
+
+  function renderItem({ item }) {
+    return <List {...item} />;
+  }
   
     return (
         <View style={styles.sectionContainer}>
         <FlatList
           data={myRegister}
-          renderItem={({ item }) => <List {...item}/>}
+          renderItem={renderItem}
           />
       </View>  
     );  
